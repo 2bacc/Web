e694ba88cd26e24ff8db04657107710d077e1f47
admin <admin@2ba.cc>
2023-01-06 21:18:55 +0800

    删除所有的提交记录

---
+++ README.md
@@ -0,0 +1,1 @@
+# IPFS Public Gateway Checker
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Checker } from './Checker'
+import gateways from './gateways.json'
+import { loadCountly } from './metrics'
+import { Log } from './Log'
+
+loadCountly()
+const log = new Log('App index')
+
+window.checker = new Checker()
+
+window.checker.checkGateways(gateways).catch((err) => {
+  log.error('Unexpected error')
+  log.error(err)
+})
