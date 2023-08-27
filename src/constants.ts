038e6d50449709784ace1a6da5cd62d2e6963d63
2bacc <admin@2ba.cc>
2023-08-27 22:48:09 +0800

    OK

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+
+const HASH_STRING = 'Hello from IPFS Gateway Checker'
+const HASH_TO_TEST = 'bafybeihk6hm4oz7rhtowz7h6kwfy57awlluhb3xmp2vl2cqmsrdziinccu'
+const IMG_HASH = 'bafybeibwzifw52ttrkqlikfzext5akxu7lz4xiwjgwzmqcpdzmp3n5vnbe' // 1x1.png
+// const IFRAME_HASH = 'bafkreifx3g6bkkwl7b4v43lvcqfo5vshbiehuvmpky2zayhfpg5qj7y3ca'
+const IPNS_PATH_TO_TEST = '/ipns/en.wikipedia-on-ipfs.org/favicon.ico'
+const TRUSTLESS_RESPONSE_TYPES = ['raw', 'car']
+const DEFAULT_IPFS_GATEWAY = 'https://ipfs.2ba.cc'
+
+export {
+  DEFAULT_IPFS_GATEWAY,
+  HASH_STRING,
+  HASH_TO_TEST,
+  // IFRAME_HASH,
+  IMG_HASH,
+  IPNS_PATH_TO_TEST,
+  TRUSTLESS_RESPONSE_TYPES,
+}
